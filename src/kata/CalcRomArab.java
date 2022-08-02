7a5fd490b6a760d02cf0bda28a0194db12e2a7a9
User <prograbota@mail.ru>
2022-08-02 20:27:56 +0300

    Commit2

--- src/kata/CalcRomArab.java
+++ src/kata/CalcRomArab.java
@@ -12,59 +12,99 @@ public class CalcRomArab {
     static char operation;
     static int result;
 
+
+
     public static void main (String[] args) {
         System.out.println("Введите выражение [2+2] или два римских числа от I до X:[V+V] + Enter ");
 //      Считываем строку userInput которую ввёл пользователь
-        String userInput = scanner.nextLine();
-//      Создаём пустой символьный массив длиной 10 символов:  under_char
-        char[] under_char = new char[10];
-//      Заполняем символьный массив символами строки которую ввел пользователь и по ходу ловим знак операции
-        for (int i = 0; i < userInput.length(); i++) {
-            under_char[i] = userInput.charAt(i);
-            if (under_char[i] == '+') {
-                operation = '+';
-            }
-            if (under_char[i] == '-') {
-                operation = '-';
-            }
-            if (under_char[i] == '*') {
-                operation = '*';
-            }
-            if (under_char[i] == '/') {
-                operation = '/';
-            }
-        }
-        String under_charString = String.valueOf(under_char);
-        String[] blacks = under_charString.split("[+-/*]");
-        String stable00 = blacks[0];
-        String stable01 = blacks[1];
-        String string03 = stable01.trim();
-        number1 = romanToNumber(stable00);
-        number2 = romanToNumber(string03);
-
-        if ((Character.isDigit(stable00.charAt(0)) & !Character.isDigit(string03.charAt(0))) || (!Character.isDigit(stable00.charAt(0)) & Character.isDigit(string03.charAt(0)))) {
-            throw new IllegalArgumentException("римские и арабские не совмещать!!!!!!!!");
-        }
-
-        if (blacks.length > 2) {
-            throw new IllegalArgumentException("не более двух значений!!!!!!!!!!");
-        }
-
-
-        if (number1 < 0 && number2 < 0) {
-            result = 0;
-        } else {
-            result = calculated(number1, number2, operation);
-            System.out.println("---Результат для римских цифр----");
-            String resultRoman = convertNumToRoman(result);
-            System.out.println(stable00 + " " + operation + " " + string03 + " = " + resultRoman);
-        }
-        number1 = Integer.parseInt(stable00);
-        number2 = Integer.parseInt(string03);
-        result = calculated(number1, number2, operation);
-        System.out.println("--Результат для арабских цифр----");
-        System.out.println(number1 + " " + operation + " " + number2 + " = " + result);
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
     }
+
+              public static String calc (String userInput) {
+
+
+
+
+
+         //     System.out.println("Введите выражение [2+2] или два римских числа от I до X:[V+V] + Enter ");
+//      Считываем строку userInput которую ввёл пользователь
+          //  String userInput = scanner.nextLine();
+
+
+//      Создаём пустой символьный массив длиной 10 символов:  under_char
+            char[] under_char = new char[10];
+//      Заполняем символьный массив символами строки которую ввел пользователь и по ходу ловим знак операции
+            for (int i = 0; i < userInput.length(); i++) {
+                under_char[i] = userInput.charAt(i);
+                if (under_char[i] == '+') {
+                    operation = '+';
+                }
+                if (under_char[i] == '-') {
+                    operation = '-';
+                }
+                if (under_char[i] == '*') {
+                    operation = '*';
+                }
+                if (under_char[i] == '/') {
+                    operation = '/';
+                }
+            }
+
+
+            String under_charString = String.valueOf(under_char);
+            String[] blacks = under_charString.split("[+-/*]");
+            String stable00 = blacks[0];
+            String stable01 = blacks[1];
+            String string03 = stable01.trim();
+
+
+            if ((Character.isDigit(stable00.charAt(0)) & !Character.isDigit(string03.charAt(0))) || (!Character.isDigit(stable00.charAt(0)) & Character.isDigit(string03.charAt(0)))) {
+                throw new IllegalArgumentException("римские и арабские не совмещать!!!!!!!!");
+            }
+
+            if (blacks.length > 2) {
+                throw new IllegalArgumentException("не более двух значений!!!!!!!!!!");
+            }
+
+
+            if (!Character.isDigit(stable00.charAt(0)) & !Character.isDigit(string03.charAt(0))) {
+                number1 = romanToNumber(stable00);
+                number2 = romanToNumber(string03);
+
+                //if (number1 < 0 && number2 < 0) {
+                //  result = 0;
+                if ((number1 < 0 || number2 < 0) && (number1 > 10 || number2 > 10)) {
+                    throw new IllegalArgumentException("вводить только положительные числа от I до X");
+                } else {
+                    if (operation == '-' && number1 < number2) {
+                        throw new IllegalArgumentException("В римском исчеслении нет отрицательных чисел");
+                    } else {
+                        result = calculated(number1, number2, operation);
+                        String resultRoman = convertNumToRoman(result);
+                        System.out.println("---Результат для римских цифр----");
+                        String asd = stable00 + " " + operation + " " + string03 + " = " + resultRoman;
+                        //System.out.println(asd);
+                        return asd;
+
+                    }
+                }
+            } else {
+                number1 = Integer.parseInt(stable00);
+                number2 = Integer.parseInt(string03);
+                if ((number1 < 0 || number2 < 0) || (number1 > 10 || number2 > 10)) {
+                    throw new IllegalArgumentException("вводить только положительные числа от 1 до 10");
+                } else {
+                    result = calculated(number1, number2, operation);
+                    System.out.println("--Результат для арабских цифр----");
+                    String asd = number1 + " " + operation + " " + number2 + " = " + result;
+                    return asd;
+                    //System.out.println(asd);
+
+                }
+            }
+        }
+
 
     private static String convertNumToRoman (int numArabian) {
         String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
